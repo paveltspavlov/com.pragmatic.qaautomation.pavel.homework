4e75f2970be5c6dcf6da5f2f0daf3202e6c3e3d1
pavel <pavel_ts_pavlov@outlook.com>
2020-11-03 21:31:19 +0200

    Homework2 task4

---
+++ com.pragmatic.qaautomation.pavel.homework/src/Homework2/TaskFourHW2.java
@@ -0,0 +1,12 @@
+package Homework2;
+
+public class TaskFourHW2 {
+
+    public static void main(String[] args) {
+
+        for (int TenToOne = 10; TenToOne > 0; TenToOne--){
+            System.out.println(TenToOne);
+        }
+
+    }
+}
