e6c91de67029c63907f354b6f9a68e451649abca
pavel <pavel_ts_pavlov@outlook.com>
2021-01-31 21:18:23 +0200

    Homework 5 Task 1 WiP

---
+++ com.pragmatic.qaautomation.pavel.homework/src/Homework5Task1/Student.java
@@ -0,0 +1,56 @@
+package Homework5Task1;
+
+public class Student {
+    String name;
+    String subject;
+    double grade;
+    int yearInCollege;
+    int age;
+    boolean isDegree;
+    double money;
+
+    Student(){
+        grade = 4.0;
+        yearInCollege = 2;
+        isDegree = false;
+        money = 0;
+    }
+
+    Student(double grade, int yearInCollege, boolean isDegree, double money){
+        this();
+        this.name = "Bai Hui";
+        this.subject = "TUT";
+        this.age = 19;
+
+    }
+
+    void upYear() {
+
+            if (isDegree) {
+                System.out.println("The student has finished his education");
+                return;
+            }
+
+            yearInCollege++;
+
+            if(yearInCollege <= 4){
+                isDegree = false;
+                System.out.println("The student is "+yearInCollege+" year in college");
+            }else{
+                isDegree=true;
+                System.out.println("The student has finished his education");
+
+            }
+
+    }
+
+    double receiveScholarship(double min, double amount){
+
+        if(grade>=min  && age<30){
+            money=+amount;
+
+        }
+        System.out.println("Student's money are "+amount);
+        return money;
+    }
+}
