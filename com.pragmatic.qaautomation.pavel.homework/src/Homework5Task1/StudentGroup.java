67c77b66422fe02355f74ee1cdd23ba828532f79
pavel <pavel_ts_pavlov@outlook.com>
2021-01-31 21:19:56 +0200

    Homework 5 Task 1 WiP

---
+++ com.pragmatic.qaautomation.pavel.homework/src/Homework5Task1/StudentGroup.java
@@ -0,0 +1,14 @@
+package Homework5Task1;
+
+public class StudentGroup{
+    String groupSubject;
+    Student[] students;
+    int freePlace;
+
+    StudentGroup(){
+        freePlace=5;
+        Student[] students = new Student[];
+        students[0]={Ivan};
+    }
+
+}
