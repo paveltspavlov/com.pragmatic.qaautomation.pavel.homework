94b75f3cca8f8405cd995d448ebd1898ba2650e4
pavel <pavel_ts_pavlov@outlook.com>
2021-02-01 23:46:09 +0200

    Homework 5 Task 1 WiP 01022021

--- com.pragmatic.qaautomation.pavel.homework/src/Homework5Task1/StudentGroup.java
+++ com.pragmatic.qaautomation.pavel.homework/src/Homework5Task1/StudentGroup.java
@@ -6,9 +6,42 @@ public class StudentGroup{
     int freePlace;
 
     StudentGroup(){
-        freePlace=5;
-        Student[] students = new Student[];
-        students[0]={};
+        this.students = new Student[5];
+        freePlace = 5;
+    }
+    StudentGroup(String subject){
+        this();
+        groupSubject = "Computer Science";
+    }
+
+    void addStudent(Student s){
+//        String subject = s.subject;
+//        Student ivan = new Student();
+//        int i = students.length;
+//        do {
+//            if (subject == groupSubject) {
+//                this.students[i] = s;
+//                i--;
+//            }
+//        }while (i<= students.length);
+        int i = students.length;
+        do{
+            if (s.subject==groupSubject){
+                students[i] = s;
+                i--;
+                freePlace--;
+                return;
+            }
+        }while(i>0);
+    }
+
+    void emptyGroup() {
+        int i = this.freePlace;
+        if (students[i]>0) {
+            students[freePlace] = null;
+            i++;
+            freePlace++;
+        }
     }
 
 }
